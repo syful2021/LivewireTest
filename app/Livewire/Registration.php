427f40a313a64580277420b495105f29ae036f29
syful2021 <mdsyfulislam20009@gmail.com>
2024-03-21 09:28:41 +0600

    insert data on database and reset field ofter submit form

--- app/Livewire/Registration.php
+++ app/Livewire/Registration.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\student;
 
 class Registration extends Component
 {
@@ -12,22 +13,34 @@ class Registration extends Component
     {
         return view('livewire.registration');
     }
-            // updated for show live Validation
+    // updated for show live Validation
     public function updated($field)
     {
-        $this->validateOnly([
+        $this->validateOnly($field, [
             'name' => 'required',
             'rollno' => 'required|min:3|max:10',
             'email' => 'required | email'
-        ])
+        ]);
     }
 
     public function submit()
     {
-        $this->validate(
+        $this->validate([
             'name' => 'required',
             'rollno' => 'required|min:3|max:10',
             'email' => 'required | email'
-        )
+        ]);
+        $student = new student;
+        $student->rollno = $this->rollno;
+        $student->name = $this->name;
+        $student->email = $this->email;
+
+        $student->save();
+        $this->resetFilter();
+    }
+    // resetFilter for reset form data after submit
+    public function resetFilter()
+    {
+        $this->reset(['rollno', 'name', 'email']);
     }
 }
